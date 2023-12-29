880457e1e36700da95022d6b9e3039d947def174
Mohamed Tarek <mohamed96tarek@hotmail.com>
2023-12-29 10:45:48 +0200

    batch commit: urls changes

--- lenme_django/api_investors/urls.py
+++ lenme_django/api_investors/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("investor/create", views.CreateInvestorView.as_view()),
     path("investor/view", views.ViewInvestorsView.as_view()),
+    path("investor/can-fund", views.MockInvestorCanFund.as_view()),
 ]
--- lenme_django/api_payment/urls.py
+++ lenme_django/api_payment/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import MonthlyPaymentView
+from .views import MonthlyPaymentView, SimulateMonthlyPaymentsView
 
-urlpatterns = [path("", MonthlyPaymentView.as_view())]
+urlpatterns = [
+    path("", MonthlyPaymentView.as_view()),
+    path("sim", SimulateMonthlyPaymentsView.as_view()),
+]
